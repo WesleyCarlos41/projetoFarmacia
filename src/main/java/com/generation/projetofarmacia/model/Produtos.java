bbba28d8a65df8037f68eaa3db97f6b4066d1686
WesleyCarlos41 <102267247+WesleyCarlos41@users.noreply.github.com>
2022-05-04 18:02:08 -0300

    Relacionamento entre tabelas

---
+++ src/main/java/com/generation/projetofarmacia/model/Produtos.java
@@ -0,0 +1,84 @@
+package com.generation.projetofarmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produtos {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private String marca;
+	
+	@NotNull
+	private String quantidade;
+	
+	@NotNull
+	private Double preco;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Categoria categoria;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(String quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
